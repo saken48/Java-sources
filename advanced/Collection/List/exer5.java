949b2adecace630aafb6b32b582b7f648de7c46a
saken48 <162938047+saken48@users.noreply.github.com>
2025-06-06 22:49:17 +0700

    Create exer5.java

---
+++ advanced/Collection/List/exer5.java
@@ -0,0 +1,14 @@
+
+import java.util.List;
+
+public class exer5 {
+    public static void main(String[] args) {
+        List<Integer> numbers = List.of(10, 20, 30, 40, 50, 60);
+
+        numbers.subList(2, 4);
+
+        System.out.println(numbers.subList(2, 4));
+
+    }
+
+}
